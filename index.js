37c10cb87245b39dc5cd8da70e1c03b83e697a7f
mmbapin <bapinmm@gmail.com>
2026-04-17 21:58:37 +0600

    jwt authentication added

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ import dotenv from "dotenv";
 import { connextDB } from "./config/db.js";
 import personRoute from "./routers/personRoute.js";
 import authRoute from "./routers/authRoute.js";
-
+import jwt from "jsonwebtoken";
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(cookieParser());
-app.use(session({
-  secret: 'sample-secret',
-  resave: false,
-  saveUninitialized: false
-}))
+// app.use(cookieParser());
+// app.use(session({
+//   secret: 'sample-secret',
+//   resave: false,
+//   saveUninitialized: false
+// }))
 
 app.use(express.json());
 
@@ -29,10 +29,17 @@ app.use('/person', personRoute);
 app.use('/auth', authRoute);
 
 app.get('/dashboard', (req, res) => {
-  if(!req.session.user){
-    return res.send("Unauthorized")
+  // if(!req.session.user){
+  //   return res.send("Unauthorized")
+  // }
+  const token = req.header('Authorization');
+  const decodedToken = jwt.verify(token, 'test#secret');
+  if(decodedToken.username){
+    res.send(`Welcome ${decodedToken.username}`)
+  }else{
+    res.send("Unauthorized")
   }
-  res.send(`Welcome ${req.session.user.userName}`)
+  // res.send(`Welcome ${req.session.user.userName}`)
 })
 
 app.listen(PORT, () => {
--- routers/authRoute.js
+++ routers/authRoute.js
@@ -1,23 +1,33 @@
 import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 const users = [];
 
-router.post('/register', (req, res) => {
-  const {userName, email, password} = req.body;
-  users.push({userName, email, password});
+router.post('/register',async (req, res) => {
+  const {username, email, password} = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  users.push({
+    username, 
+    email, 
+    password: hashedPassword
+  });
   res.status(201).json({message: "User registered", user: users});
+  console.log("User List:", users)
 })
 
-router.post('/login', (req, res) => {
+router.post('/login',async (req, res) => {
+  console.log("User List:", users)
   const {email, password} = req.body;
-  const user = users.find(user => user.email === email && user.password === password);
-  if(!user || password !== user.password){
+  const user = users.find(user => user.email === email);
+  console.log("User :", user)
+  if(!user || !(await bcrypt.compare(password, user.password))){
     return res.status(401).json({message: "Invalid credentials"});
   }
-  req.session.user = user;
-  res.status(200).json({message: "User logged in", user});
+  const token = jwt.sign({username: user.username}, 'test#secret', {expiresIn: '1h'});
+  res.status(200).json({message: "User logged in", user, token});
 })
 
 router.post('/logout', (req, res) => {
